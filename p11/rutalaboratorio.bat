e1f1b7f2f0d349ce8d66f05c69cb94f6684dd297
Nicolás Pérez Fernández <UO306041@uniovi.es>
2026-02-04 19:54:22 +0100

    Modificacion de Clase

--- p11/rutalaboratorio.bat
+++ p11/rutalaboratorio.bat
@@ -1,2 +1,2 @@
-set path=C:\Program Files\Java\<jdk>\bin
-set classpath=C:\Users\<loquesea>\<repositorio github>\p11
+set path=C:\Program Files\Java\jdk-24\bin
+set classpath=C:\Users\uo306041\Desktop\alg_PerezFernandezNicolasUO306041\p11
